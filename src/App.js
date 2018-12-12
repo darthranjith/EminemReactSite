866fd2179673df72805e955f787c78f4edf90b2b
Ranjith Varatharajan <srivaranjith@gmail.com>
2018-12-12 22:03:52 +0530

    Added Venue Details and Sticky Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./resources/styles.css";
 import Header from './components/header_footer/header';
 import Featured from './components/featured';
+import VenueInfo from './components/venue_info';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App" style={{ background: 'cornflowerblue' }}>
         <Header></Header>
         <Featured />
+        <VenueInfo />
       </div>
     );
   }
